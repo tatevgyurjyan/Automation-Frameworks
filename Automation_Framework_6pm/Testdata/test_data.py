cb93fb0911bccde76bf11d0e3b3fe7214511acbc
tatevgyurjyan <tgyurjyan@yahoo.com>
2021-12-20 20:47:47 +0100

    API test automation project

--- Automation_Framework_6pm/Testdata/test_data.py
+++
@@ -1,4 +0,0 @@
-
-log_file_name = "test_log.txt"
-search_product_name = "Sunglasses"
-brand_name = "Calvin Klein"
--- Automation_Framework_6pm/config.py
+++
@@ -1,3 +0,0 @@
-url = "https://www.6pm.com/"
-username = "tgyurjyan@yahoo.com"
-password = "123456"
